fb1b9e4a238da3de237b4ca5a3a0a950a7c7e946
Javier <35864946+jmagit@users.noreply.github.com>
2026-07-09 15:01:34 +0200

    microservicio completo

---
+++ catalogo/src/main/java/com/example/presentation/resources/ETagTools.java
@@ -0,0 +1,33 @@
+package com.example.presentation.resources;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ETagTools {
+
+	public static String generate(Object content) {
+    	if(content == null)
+    		throw new IllegalArgumentException("El contenido no puede ser nullo");
+        byte[] digest = Integer.toHexString(content.hashCode()).getBytes(StandardCharsets.UTF_8);
+		try {
+			digest = MessageDigest.getInstance("MD5").digest(digest);
+		} catch (NoSuchAlgorithmException e) {
+			log.warn("ETag sin hash"); 
+		}
+        return "\"" + HexFormat.of().formatHex(digest) + "\"";
+    }
+    public static boolean ifMatch(Object content, String ifMatch) {
+    	if(ifMatch == null) return false;
+    	if(!ifMatch.startsWith("\"")) ifMatch = "\"" + ifMatch;
+    	if(!ifMatch.endsWith("\"")) ifMatch += "\"";
+       return generate(content).equals(ifMatch);
+    }
+    public static boolean ifNoneMatch(Object content, String ifNoneMatch) {
+        return ifNoneMatch != null && !ifMatch(content, ifNoneMatch);
+    }
+}
